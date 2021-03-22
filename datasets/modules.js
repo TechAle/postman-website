9e4c206a53482b701a49b3ee3c78cb911b8dc742
TechAle <alessandro.condello.email@gmail.com>
2021-03-22 20:34:47 +0100

    added who section, refractor everything

---
+++ datasets/modules.js
@@ -0,0 +1,70 @@
+var dictionaryModules = {
+    "Player" : {
+        "autoDisconnect": "automatically disconnects when u reach a set health.",
+        "autoMine": "automatically mines.",
+        "esp's" : "makes entities and storages visible (lots of customization).",
+        "freecam" : "allows u to leave ur body and explore the enviorment with a free camera.",
+        "fullBright" : "fully brightens everything.",
+        "holeEsp" : "draws esp in bedrock and obsidian holes for better cpvp.",
+        "lowOffHand" : "allows u to chose the visual height of ur offhand.",
+        "noRender" : "stops certain events from rendering.",
+        "peek" : "allows u to see whats in a shulker by hovering ur mouse over it.",
+        "tracers" : "draws a line to certain entities.",
+        "viewModel" : "allows u to change how items render in ur hand."
+    },
+    "Pvp" : {
+        "autoArmor" : "automatically equips the best armor in ur inventory.",
+        "autoClicker" : "clicks really fast wen held.",
+        "autoCrystal" : "automatically crystals your opponent.",
+        "autoGap" : "automatically eats any gapples in ur hands.",
+        "blink" : "temporarily creates a second player to throw off opponents.",
+        "criticals" : "always land critical hits without having to jump.",
+        "fastUse" : "allows u to use things faster like shoot bows and throw xp bottles.",
+        "footExp" : "automatically throws xp bottles at your feet.",
+        "holeTp" : "automatically sucks u into a bedrock or obsidian hole.",
+        "killAura" : "automatically hits certain entities.",
+        "smartOffHand" : "allows u to use crystals and gaps smartly without a totem in ur hand 24/7.",
+        "surround" : "autmatically surrounds u in obsidian."
+    },
+    "Exploits" : {
+        "antiHunger" : "reduces the amount of hunger you loose.",
+        "antiSwing" : "prevents swinging server side.",
+        "portalGodMode" : "gives you god mode in portals.",
+        "timer" : "allows you to edit your player's timer."
+    },
+    "Movements" : {
+        "autoWalk" : "autmatically holds w key for u to walk.",
+        "inventoryMove" : "allows you to move while in a gui screen.",
+        "jesus" : "lets u walk on water like jesus himself.",
+        "noSlow" : "prevents certain events from slowing you down, e.g. eating, soulsand, webs, and slimeblocks.",
+        "reverseStep" : "automatically sucks u down when stepping down a block.",
+        "safeWalk" : "prevents u from falling off the edges of blocks.",
+        "speed" : "allows you to outrun his bullet.",
+        "sprint" : "automatically sprints when holding your foward key.",
+        "step" : "tp's you to the top of a block so you don't have to jump."
+    },
+    "Hud" : {
+        "watermark" : "shows the client name and version.",
+        "totems's" : "shows you how many totems you currently have on your player.",
+        "ping" : "shows your current ping.",
+        "frames" : "shows the current frame rate of your game.",
+        "autoCrystalInfo" : "tells you if your auto crystal is off or on.",
+        "killAuraInfo" : "tells you if your kill aura is off or on.",
+        "surroundInfo" : "tells you if your surround is off or on.",
+        "arraylist" : "shows you all current enabled modules.",
+        "inventoryViewer" : "gives you a hud of your inventory.",
+        "playerModel" : "shows ur player on the hud.",
+        "coords" : "shows you your current coordinates in game.",
+        "armorHud" : "gives you a display of your armor along with the durability of each armor piece.",
+        "hudEditor" : "allows you to edit the hud of the client."
+    },
+    "Client" : {
+        "clientFont" : "allows u to change the font of the client.",
+        "capes" : "allows u to see your own, and other peoples postman capes (u can get a cape if u have ++).",
+        "discordRp" : "shows you are playing postman on discord.",
+        "clickGui" : "gives you multiple setting to customize the clickGui to your liking.",
+        "tabGui" : "allows you to navigate the client through your arrow keys.",
+        "mainMenuInfo" : "shows postman info on the main menu of Minecraft."
+    }
+
+}
---
+++ datasets/playerDev.js
@@ -0,0 +1,5 @@
+var playerDev = {
+    "000srgantmoomoo" : "Owner, he created and he is developing the client",
+    "001nocatsnolife" : "Developer, he created the website and contributed to postman",
+    "002lukflug" : "Developer, creator of panelstudio, the library that postman uses for displaying things"
+}
